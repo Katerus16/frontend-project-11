70fa66bda6025decf282a354a8e9e1e55b758059
katerus16 <sherryyy@yandex.ru>
2025-03-31 15:15:44 +0300

    step 1

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  // plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    static: path.resolve(__dirname),
+    port: 8080,
+    hot: true
+  },
+};
