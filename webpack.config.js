213ba4c6bf82f1379f668392dc8e1e08bc6401c2
katerus16 <sherryyy@yandex.ru>
2025-04-08 14:28:09 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: 'development',
+  ignoreWarnings: [
+    { module: /style\.scss/ },
+  ],
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
@@ -14,17 +20,26 @@ export default {
       },
     ],
   },
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
-  // plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   devServer: {
-    static: path.resolve(__dirname),
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
     port: 8080,
     hot: true,
+    client: {
+      overlay: false,
+    },
   },
 };
